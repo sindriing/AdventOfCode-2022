68aa9fa5d3b29baecfdae4628b1559fb233e1f7f
Sindri Ingolfsson <singolfs@cisco.com>
2022-12-10 20:05:13 +0100

    day 8b

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -19,9 +19,112 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn part_b(mut lines: &mut Lines) {
+    println!("Part B");
+    let mut grid: Vec<Vec<TreeHouse>> = construct_grid(&mut lines);
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            look_left(&mut grid[i], j);
+            look_right(&mut grid[i], j);
+        }
+    }
+    grid = transpose(grid);
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            look_left(&mut grid[i], j);
+            look_right(&mut grid[i], j);
+        }
+    }
+    // Don't really need to turn it back but I find it nicer
+    // to return it to the original orientation
+    grid = transpose(grid);
+
+    // print_trees(&grid);
+    // println!("");
+    // print_treehouse_scores(&grid);
+
+    let best_score = find_best_scoring_treehouse(&grid);
+    println!("Best score: {}", best_score);
+}
+
+fn find_best_scoring_treehouse(grid: &Vec<Vec<TreeHouse>>) -> i32 {
+    let mut best_score = 0;
+    for row in grid {
+        for treehouse in row {
+            if treehouse.score > best_score {
+                best_score = treehouse.score;
+            }
+        }
+    }
+    best_score
+}
+
+fn look_left(trees: &mut Vec<TreeHouse>, origin: usize) {
+    let mut i = origin as i32 -1;
+    let mut score = 0;
+    while i >= 0 {
+        // Tree is smaller than us
+        if trees[i as usize].size < trees[origin].size {
+            score += 1;
+        } else {
+            // we've found a tree that's bigger than us
+            score += 1;
+            break;
+        }
+        i -= 1;
+    }
+    trees[origin].score *= score;
+}
+
+fn look_right(trees: &mut Vec<TreeHouse>, origin: usize) {
+    let mut i = origin as i32 + 1;
+    let mut score = 0;
+    while i < trees.len() as i32 {
+        if trees[i as usize].size < trees[origin].size {
+            score += 1;
+        } else {
+            score += 1;
+            break;
+        }
+        i += 1;
+    }
+    trees[origin].score *= score;
+}
+
 struct Tree {
     size: i32,
     seen: bool,
+}
+
+impl Trunk for Tree {
+    fn new(s: i32) -> Tree {
+        Tree { size: s, seen: false, }
+    }
+    fn size(&self) -> i32 {
+        self.size
+    }
+}
+
+trait Trunk {
+    // Static method signature; `Self` refers to the implementor type
+    fn new(s: i32) -> Self;
+    fn size(&self) -> i32;
+}
+
+struct TreeHouse {
+    size: i32,
+    score: i32,
+}
+
+impl Trunk for TreeHouse {
+    fn new(s: i32) -> TreeHouse {
+        TreeHouse { size: s, score: 1 }
+    }
+    fn size(&self) -> i32 {
+        self.size
+    }
 }
 
 fn walk_grid(grid: &mut Vec<Vec<Tree>>) {
@@ -50,15 +153,27 @@ fn count_seen(grid: &Vec<Vec<Tree>>) -> i32 {
     return count;
 }
 
-fn print_grid(grid: &Vec<Vec<Tree>>) {
+#[allow(dead_code)]
+fn print_trees<T: Trunk>(grid: &Vec<Vec<T>>) {
     for row in grid {
         for col in row {
-            print!("{}", col.size);
+            print!("{}", col.size());
         }
         println!("");
     }
 }
 
+#[allow(dead_code)]
+fn print_treehouse_scores(grid: &Vec<Vec<TreeHouse>>) {
+    for row in grid {
+        for col in row {
+            print!("{}", col.score);
+        }
+        println!("");
+    }
+}
+
+#[allow(dead_code)]
 fn print_seen(grid: &Vec<Vec<Tree>>) {
     for row in grid {
         for tree in row {
@@ -74,15 +189,12 @@ fn print_seen(grid: &Vec<Vec<Tree>>) {
     }
 }
 
-fn construct_grid(lines: &mut Lines) -> Vec<Vec<Tree>> {
+fn construct_grid<T: Trunk>(lines: &mut Lines) -> Vec<Vec<T>> {
     let mut grid = Vec::new();
     for line in lines {
         let temp = line
             .chars()
-            .map(|s| Tree {
-                size: s.to_digit(10).unwrap() as i32,
-                seen: false,
-            })
+            .map(|s| T::new(s.to_digit(10).unwrap() as i32))
             .collect();
         grid.push(temp);
     }
@@ -103,7 +215,7 @@ fn part_a(mut lines: &mut Lines) {
 
     grid = mirror(grid);
     walk_grid(&mut grid);
-    print_grid(&grid);
+    print_trees(&grid);
     print_seen(&grid);
 
     let seen = count_seen(&grid);
@@ -130,8 +242,4 @@ fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
                 .collect::<Vec<T>>()
         })
         .collect()
-}
-
-fn part_b(_: &mut Lines) {
-    println!("Part B");
 }
